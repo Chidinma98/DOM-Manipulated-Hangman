ede34fc507e9f9bb4083c38c22b91ed709e321ea
MaryAnn Eleanya <meleanya@gmail.com>
2019-05-07 11:10:16 -0400

    css html and some javascript

--- project_1.js
+++ project_1.js
@@ -1,3 +1,45 @@
 // Arrays for Words & Letters
-
 let category = ['Beach', 'Sun', 'Pool', 'Vacation']
+let guessWord = document.querySelector('#word-field').value
+let letterButton = document.querySelector('#letter')
+let wordButton = document.querySelector('#word')
+
+
+
+
+
+let wordsPlay = ()=> {
+
+let randWord = Math.floor((Math.random() * 4))
+let words = document.querySelector('.blank-Words');
+
+
+let loopWord = category[randWord]
+let dash = document.createElement('p');
+
+
+for (let i = 0; i < loopWord.length; i++) {
+   
+   let dash = document.createElement('h1');
+   words.appendChild(dash)
+   dash.textContent = ('-')
+
+}
+
+}
+
+
+
+
+wordsPlay();
+
+wordButton.addEventListener('click', function checkWords(guessWord){
+guessWord.preventDefault();
+
+
+console.log(guessWord);
+
+
+})
+
+
