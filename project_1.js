44d5dc56818671bf0a80e74a48e14cc6af0a9967
MaryAnn Eleanya <meleanya@gmail.com>
2019-05-08 18:25:58 -0400

    switches players)

--- project_1.js
+++ project_1.js
@@ -1,13 +1,26 @@
 // Arrays for Words & Letters
+
+let objTopic ={
+
+Summer: ['n'],
+
+Winter: ['u'],
+
+Sports: ['l'],
+}
+
+let playerNumber = 4;
+let altPlayer = document.querySelector('#userName')
+// console.log(objTopic.keys)
+
+function reFresh() {
+    window.location.reload()
+}
+
+
 let category = ['BEACH', 'SUN', 'POOL', 'VACATION', 'SWIMMING']
 
-
-
-
-
-
-
-
+let locations = [];
 
 let letterButton = document.querySelector('#letter')
 let wordButton = document.querySelector('#word')
@@ -16,26 +29,31 @@ let randWord = Math.floor((Math.random() * 5))
 let loopWord = category[randWord]
 let lArray = (loopWord.split(''))
 
+let player0Points = 0
+let player1Points = 0
+let player2Points = 0
+
+let vPoints = document.querySelector('#value')
+
 
 let wordsPlay = () => {
     let words = document.querySelector('.blank-Words');
-    
+
     for (let i = 0; i < loopWord.length; i++) {
         let dash = document.createElement('h2');
 
         words.appendChild(dash)
        
         dash.textContent += ('-')
-
     }
     console.log(words)
-
 }
-
 
 wordsPlay();
 
 //This Function checks If The Word Matches
+
+let wordFunction = () => {
 
 wordButton.addEventListener('click', function checkWords() {
     event.preventDefault();
@@ -55,28 +73,46 @@ wordButton.addEventListener('click', function checkWords() {
         alert(`You Have Solved The Puzzle`)
         //Function that shows you the points for Each players in the Game.
         //Another Function that restarts the game
+        
+        if (playerNumber === 0) {
+            player0Points += 10
+            vPoints.innerHTML = `Points: ${player0Points}`
+        }
+        else if (playerNumber === 1) {
+            player1Points += 10
+            vPoints.innerHTML = `Points: ${player1Points}`
+        }
+
+        else if (playerNumber === 2) {
+            player2Points += 10
+            vPoints.innerHTML = `Points: ${player2Points}`
+        }
+
     }
     else {
-        console.log(false)
+        alert('Incorect You loose 2 points')
     }
    
  })
 
-console.log(document.querySelectorAll('h2')[0])
-
-let byeFunc = () => {
-    console.log('Not A Match')
-
-///
+// console.log(document.querySelectorAll('h2')[0])
 }
 
-let locations = [];
+
+
+
+let letterFunction = () => {
 
 letterButton.addEventListener('click', function checkLetters() {
     event.preventDefault();
     let guessLetter = document.querySelector('#word-field').value;
     let uGuessLetter = String(guessLetter.toUpperCase());
 
+    let byeFunc = () => {
+        alert('Incorecct, You Lose 1 Point')
+    
+    
+    }
     let hiFunc = () => {
         
         for (let i = 0; i < lArray.length; i++) {
@@ -101,6 +137,22 @@ letterButton.addEventListener('click', function checkLetters() {
     }
     if (lArray.some(checkMatch)) {
         hiFunc();
+        if (playerNumber === 0) {
+            player0Points += 1
+            vPoints.innerHTML = `Points: ${player0Points}`
+        }
+        else if (playerNumber === 1) {
+            player1Points += 1
+            vPoints.innerHTML = `Points: ${player1Points}`
+        }
+
+        else if (playerNumber === 2) {
+            player2Points += 1
+            vPoints.innerHTML = `Points: ${player2Points}`
+        }
+
+    
+    
     }
     else {
         byeFunc();
@@ -108,24 +160,39 @@ letterButton.addEventListener('click', function checkLetters() {
 
 })
 
-
-
-// Class to Instantiate the Players
-
-class Player {
-    constructor (name) {
-        this.name = name;
-        this.points= 0;
-    }
-
-    totalPoint() {
-        // points gained + points lost
-    }
-
 }
 
 
-let playerName = () => {
-    let name = document.querySelector('#person')
 
-};
+//Player Switch Function
+
+
+
+let switchPlayers = () => {
+
+
+
+let currentPlayer = () => {
+playerNumber = ((playerNumber + 1) % 3)
+altPlayer.innerHTML = `player: ${playerNumber}`;
+// return((`player: ${playerNumber}`))
+console.log(playerNumber)
+}
+
+let run = () => {
+    currentPlayer();
+    
+}
+setInterval(run, 10000)
+}
+
+switchPlayers();
+// let altPlayer = document.querySelector('#userName')
+
+letterFunction();
+wordFunction();
+
+// Class to Instantiate the Players
+
+
+
